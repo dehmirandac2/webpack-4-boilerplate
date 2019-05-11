6679dc51b938a66b308455a62bfcb672d2dc4348
Deborah Miranda Casanova Costa <dehmirandac2@Deborahs-MacBook-Pro.local>
2019-05-11 19:15:50 -0300

    feat: primeiro commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import importedFunction from "./js/my-function";
+
+const testTranspile = () => console.log("arrow function");
+
+testTranspile();
+importedFunction();
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/* plugin que vai adicionar o script gerado no html */
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./build"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html",
+        })
+    ]
+}
